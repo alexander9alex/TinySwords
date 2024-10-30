6ab6139c526d86b8e55a9d983028025d74f0fa44
Zyuzin Alexander <alexander9alex@mail.ru>
2024-10-30 19:55:35 +0500

    Make buttons actions of main menu

---
+++ src/TinySwords/Assets/Code/UI/Windows/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using Code.Infrastructure.States.GameStates;
+using Code.Infrastructure.States.StateMachine;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Code.UI.Windows
+{
+  public class MainMenu : MonoBehaviour
+  {
+    public Button PlayButton;
+    public Button ExitGameButton;
+    
+    private IGameStateMachine _gameStateMachine;
+
+    [Inject]
+    private void Construct(IGameStateMachine gameStateMachine) =>
+      _gameStateMachine = gameStateMachine;
+
+    private void Awake()
+    {
+      PlayButton.onClick.AddListener(StartGame);
+      ExitGameButton.onClick.AddListener(ExitGame);
+    }
+
+    private void StartGame() =>
+      _gameStateMachine.Enter<LoadGameState>();
+
+    private void ExitGame() =>
+      Application.Quit();
+  }
+}
