ccdb2d427387d42417ce26fee5109026a2b6f0f0
Zyuzin Alexander <alexander9alex@mail.ru>
2024-10-30 08:41:57 +0500

    Create bootstrap installer

---
+++ src/TinySwords/Assets/Code/Infrastructure/Installers/BootstrapInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Code.Infrastructure.States.Factory;
+using Code.Infrastructure.States.StateMachine;
+using Zenject;
+
+namespace Code.Infrastructure.Installers
+{
+  public class BootstrapInstaller : MonoInstaller
+  {
+    public override void InstallBindings()
+    {
+      BindStateFactory();
+      BindStateMachine();
+    }
+
+    private void BindStateFactory() =>
+      Container.Bind<IStateFactory>().To<StateFactory>().AsCached();
+
+    private void BindStateMachine() =>
+      Container.Bind<IGameStateMachine>().To<IGameStateMachine>().AsCached();
+  }
+}
