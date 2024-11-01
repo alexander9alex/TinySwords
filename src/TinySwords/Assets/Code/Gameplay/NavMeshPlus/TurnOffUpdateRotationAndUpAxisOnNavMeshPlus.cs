10baa34b42be1dfbedf4d12addb7a2e25236ebfe
Zyuzin Alexander <alexander9alex@mail.ru>
2024-11-01 13:08:36 +0500

    Change blue knight prefab

---
+++ src/TinySwords/Assets/Code/Gameplay/NavMeshPlus/TurnOffUpdateRotationAndUpAxisOnNavMeshPlus.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Code.Gameplay.NavMeshPlus
+{
+  public class TurnOffUpdateRotationAndUpAxisOnNavMeshPlus : MonoBehaviour
+  {
+    private void Awake()
+    {
+      NavMeshAgent agent = GetComponent<NavMeshAgent>();
+      agent.updateRotation = false;
+      agent.updateUpAxis = false;
+    }
+  }
+}
