0c2332d52914e0d9aa79851a6881a9afe56b0b26
Zyuzin Alexander <alexander9alex@mail.ru>
2024-12-10 07:32:06 +0500

    Stub for missing components validator

---
+++ src/TinySwords/Assets/Code/Tools/Validator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Code.Tools
+{
+  public abstract class Validator
+  {
+    [MenuItem("Tools/Validation/Missing Components")]
+    public static void FindMissingComponents() =>
+      Debug.Log("Hello from Validator!");
+  }
+}
