8f8b9da9cc85427d09fd5aa730ed9ff054cd78a3
Zyuzin Alexander <alexander9alex@mail.ru>
2024-12-10 07:38:36 +0500

    Iterated active scene game objects

--- src/TinySwords/Assets/Code/Tools/Validator.cs
+++ src/TinySwords/Assets/Code/Tools/Validator.cs
@@ -1,12 +1,33 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Code.Tools
 {
   public abstract class Validator
   {
     [MenuItem("Tools/Validation/Missing Components")]
-    public static void FindMissingComponents() =>
+    public static void FindMissingComponents()
+    {
       Debug.Log("Hello from Validator!");
+
+      Scene scene = SceneManager.GetActiveScene();
+
+      Queue<GameObject> gameObjectQueue = new Queue<GameObject>(scene.GetRootGameObjects());
+
+      while (gameObjectQueue.Count > 0)
+      {
+        GameObject gameObject = gameObjectQueue.Dequeue();
+        
+        FindMissingComponents(gameObject);
+
+        foreach (Transform child in gameObject.transform)
+          gameObjectQueue.Enqueue(child.gameObject);
+      }
+    }
+
+    private static void FindMissingComponents(GameObject gameObject) =>
+      Debug.Log(gameObject.name);
   }
 }
