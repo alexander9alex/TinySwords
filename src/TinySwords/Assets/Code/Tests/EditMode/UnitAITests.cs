3e6c4ae742a4b60d3381b9a501351cc49f0a9fcb
Zyuzin Alexander <alexander9alex@mail.ru>
2024-12-12 07:42:10 +0500

    Test in which unit has target and unit should move to target

--- src/TinySwords/Assets/Code/Tests/EditMode/UnitAITests.cs
+++ src/TinySwords/Assets/Code/Tests/EditMode/UnitAITests.cs
@@ -1,10 +1,12 @@
 ﻿using Code.Common.Entities;
+using Code.Common.Extensions;
 using Code.Gameplay.Features.Units.Data;
 using Code.Gameplay.UtilityAI;
 using Code.Gameplay.UtilityAI.Brains;
 using Code.Gameplay.UtilityAI.Components;
 using FluentAssertions;
 using NUnit.Framework;
+using UnityEngine;
 
 namespace Code.Tests.EditMode
 {
@@ -27,6 +29,31 @@ namespace Code.Tests.EditMode
 
       // Assert
       decision.UnitDecisionTypeId.Should().Be(UnitDecisionTypeId.Stay);
+    }
+
+    [Test]
+    public void WhenUnitHasTarget_ThenUnitAIShouldMakeMoveToTargetDecision()
+    {
+      // Arrange
+      GameContext gameContext = Contexts.sharedInstance.game;
+      IUnitAI unitAI = UnitAI(gameContext);
+
+      GameEntity enemy = CreateEntity.Empty()
+          .AddId(0)
+          .AddWorldPosition(Vector2.one)
+          .With(x => x.isAlive = true);
+      
+      GameEntity unit = CreateEntity.Empty()
+        .AddWorldPosition(Vector2.zero)
+        .AddTargetBuffer(new() { enemy.Id })
+        .AddReachedTargetBuffer(new())
+        .AddAllyBuffer(new());
+
+      // Act
+      UnitDecision decision = unitAI.MakeBestDecision(unit);
+
+      // Assert
+      decision.UnitDecisionTypeId.Should().Be(UnitDecisionTypeId.MoveToTarget);
     }
 
     private static IUnitAI UnitAI(GameContext gameContext)
