25e8c225545f46c06f5dfc90fe6b31075f13c937
Zyuzin Alexander <alexander9alex@mail.ru>
2024-12-12 18:36:50 +0500

    Registered legs position for knight

---
+++ src/TinySwords/Assets/Code/Gameplay/Features/Units/Registrars/LegsPositionRegistrar.cs
@@ -0,0 +1,19 @@
+﻿using Code.Gameplay.Common.Registrars;
+using UnityEngine;
+
+namespace Code.Gameplay.Features.Units.Registrars
+{
+  public class LegsPositionRegistrar : EntityComponentRegistrar
+  {
+    public Transform LegsPosition;
+
+    public override void RegisterComponents() =>
+      Entity.AddLegsPosition(LegsPosition.position);
+
+    public override void UnregisterComponents()
+    {
+      if (Entity.hasLegsPosition)
+        Entity.RemoveLegsPosition();
+    }
+  }
+}
