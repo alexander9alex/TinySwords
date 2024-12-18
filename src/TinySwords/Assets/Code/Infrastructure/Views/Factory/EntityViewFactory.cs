218c93547fa7aebb4b7a2bb1b7258066b913f53c
Zyuzin Alexander <alexander9alex@mail.ru>
2024-12-18 08:00:20 +0500

    Spawned entity view on the scene, not in destroy on load

--- src/TinySwords/Assets/Code/Infrastructure/Views/Factory/EntityViewFactory.cs
+++ src/TinySwords/Assets/Code/Infrastructure/Views/Factory/EntityViewFactory.cs
@@ -3,20 +3,22 @@ using Zenject;
 
 namespace Code.Infrastructure.Views.Factory
 {
-  class EntityViewFactory : IEntityViewFactory
+  public class EntityViewFactory : IEntityViewFactory
   {
     private readonly IInstantiator _instantiator;
-    private readonly Vector3 _farAway = new Vector3(9999, 9999, 9999);
+    private readonly Vector3 _farAway = new(9999, 9999, 9999);
+    private Transform Parent => _parent ??= new GameObject("EntityParent").transform;
+    private Transform _parent;
 
     public EntityViewFactory(IInstantiator instantiator) =>
       _instantiator = instantiator;
 
     public EntityBehaviour CreateViewFromPrefab(GameEntity entity)
     {
-      EntityBehaviour view = _instantiator.InstantiatePrefabForComponent<EntityBehaviour>(entity.ViewPrefab, _farAway, Quaternion.identity, null);
-      
+      EntityBehaviour view = _instantiator.InstantiatePrefabForComponent<EntityBehaviour>(entity.ViewPrefab, _farAway, Quaternion.identity, Parent);
+
       view.SetEntity(entity);
-      
+
       return view;
     }
   }
