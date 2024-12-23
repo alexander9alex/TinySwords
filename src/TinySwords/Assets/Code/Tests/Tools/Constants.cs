4de8ebf29edc87bc0c60054cd0934d0fb3caa538
Zyuzin Alexander <alexander9alex@mail.ru>
2024-12-23 19:20:58 +0500

    Changed max position delta (to tests)

--- src/TinySwords/Assets/Code/Tests/Tools/Constants.cs
+++ src/TinySwords/Assets/Code/Tests/Tools/Constants.cs
@@ -3,6 +3,6 @@
   public static class Constants
   {
     public const string EmptyTestScenePath = "Assets/Scenes/ForTesting/EmptyScene.unity";
-    public const float MaxPositionDelta = 0.025f;
+    public const float MaxPositionDelta = 0.15f;
   }
 }
