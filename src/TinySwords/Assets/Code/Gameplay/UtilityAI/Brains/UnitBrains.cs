aad5874a8efab75ab57cd1fee8a16750dea829dd
Zyuzin Alexander <alexander9alex@mail.ru>
2025-03-05 19:12:48 +0500

    Refactored a bit

--- src/TinySwords/Assets/Code/Gameplay/UtilityAI/Brains/UnitBrains.cs
+++ src/TinySwords/Assets/Code/Gameplay/UtilityAI/Brains/UnitBrains.cs
@@ -7,34 +7,34 @@ namespace Code.Gameplay.UtilityAI.Brains
   {
     private readonly Convolutions _convolutions;
 
-    private readonly When When;
-    private readonly GetInput GetInput;
-    private readonly Score Score;
+    private readonly When _when;
+    private readonly GetInput _getInput;
+    private readonly Score _score;
 
     public UnitBrains(IBrainsComponents brainsComponents)
     {
-      When = brainsComponents.When;
-      GetInput = brainsComponents.GetInput;
-      Score = brainsComponents.Score;
+      _when = brainsComponents.When;
+      _getInput = brainsComponents.GetInput;
+      _score = brainsComponents.Score;
 
       _convolutions = new()
       {
-        { When.DecisionIsStay, GetInput.HasEndDestination, Score.IfFalseThen(+10), "Has not End Destination" },
+        { _when.DecisionIsStay, _getInput.HasEndDestination, _score.IfFalseThen(+10), "Has not End Destination" },
 
-        { When.DecisionIsMove, GetInput.UserCommandIsMove, Score.IfTrueThen(+1000), "User Command is Move" },
-        { When.DecisionIsMove, GetInput.HasEndDestination, Score.IfTrueThen(+15), "Has End Destination" },
+        { _when.DecisionIsMove, _getInput.UserCommandIsMove, _score.IfTrueThen(+1000), "User Command is Move" },
+        { _when.DecisionIsMove, _getInput.HasEndDestination, _score.IfTrueThen(+15), "Has End Destination" },
 
-        { When.DecisionIsMoveToTarget, GetInput.HasTargets, Score.IfTrueThen(+20), "Has Targets" },
-        { When.DecisionIsMoveToTarget, GetInput.PercentageDistanceToTarget, Score.ScaledByReversed(50), "Move to Nearest Target" },
+        { _when.DecisionIsMoveToTarget, _getInput.HasTargets, _score.IfTrueThen(+20), "Has Targets" },
+        { _when.DecisionIsMoveToTarget, _getInput.PercentageDistanceToTarget, _score.ScaledByReversed(50), "Move to Nearest Target" },
 
-        { When.DecisionIsMoveToAllyTarget, GetInput.HasTargets, Score.IfFalseThen(+20), "Has not own Targets" },
+        { _when.DecisionIsMoveToAllyTarget, _getInput.HasTargets, _score.IfFalseThen(+20), "Has not own Targets" },
 
-        { When.DecisionIsMoveToAimedTarget, GetInput.CanReachToTarget, Score.IfFalseThen(+1000), "Can not Reach to Target" },
+        { _when.DecisionIsMoveToAimedTarget, _getInput.CanReachToTarget, _score.IfFalseThen(+1000), "Can not Reach to Target" },
 
-        { When.DecisionIsAttack, GetInput.CanReachToTarget, Score.IfTrueThen(+100), "Can Reach to Target" },
-        { When.DecisionIsAttack, GetInput.PercentageDistanceToReachedTarget, Score.ScaledByReversed(50), "Attack Nearest Target" },
+        { _when.DecisionIsAttack, _getInput.CanReachToTarget, _score.IfTrueThen(+100), "Can Reach to Target" },
+        { _when.DecisionIsAttack, _getInput.PercentageDistanceToReachedTarget, _score.ScaledByReversed(50), "Attack Nearest Target" },
 
-        { When.DecisionIsAttackAimedTarget, GetInput.CanReachToTarget, Score.IfTrueThen(+1500), "Can Reach to Target" },
+        { _when.DecisionIsAttackAimedTarget, _getInput.CanReachToTarget, _score.IfTrueThen(+1500), "Can Reach to Target" },
       };
     }
 
